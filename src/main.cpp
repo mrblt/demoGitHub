60dae0c1fa39aa1ac31381e9f0984514f50c1530
mrblt <mrblt2006@gmail.com>
2026-01-26 17:23:29 +0100

    exemple 2 mais le programme n'est toujours pas bon, les taches se mettent a 0 directement

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
-#include <Arduino.h>
 
+#include <Arduino.h>
+TaskHandle_t hMaTache1=NULL;
+void maTache1(void *parametres)
+{
+ int i = 0;
+ while (1) // boucle infinie
+ {
+ Serial.printf("maTache1 %4d\n", i++);
+ if(i<5)
+ {
+  vTaskSuspend(hMaTache1);
+ }
+  else if(i<15)
+ {
+  vTaskResume(hMaTache1);
+ }
+ delay(500);
+ }
+}
+TaskHandle_t hMaTache2=NULL;
+void maTache2(void *parametres)
+{
+ int i = 0;
+ while (1) // boucle infinie
+ {
+ Serial.printf("maTache2 %4d\n", i++);
+ if(i<10)
+ {
+  vTaskDelete(hMaTache2);
+ }
+ delay(2000);
+ }
+}
 void setup()
 {
- // put your setup code here, to run once:
-}
+ Serial.begin(115200);
+ while (!Serial);
+ Serial.printf("Départ\n");
+ xTaskCreate(maTache1,"Ma tâche 1",10000, NULL, 1,&hMaTache1); 
+ xTaskCreate(maTache2,"Ma tâche 2",10000,NULL,1,&hMaTache2);
 
+}
 void loop()
 {
-  int i = 0;
-  for (i = 0; i < 10; i++)
-  {
-    Serial.print(i); // put your main code here, to run repeatedly:
-    Serial.print("apppppppppppapaappapapapapapaaaaa"); 
-  }
+ static int i = 0;
+ Serial.printf("Loop %4d\n", i++);
+ delay(1000);
 }
-
