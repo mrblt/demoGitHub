3ac85deb59c24eab8cf23a1ea23b4e19688b9c3b
mrlbt <mrblt2006@gmail.com>
2026-01-28 12:26:29 +0100

    exemple 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,48 @@
-//Exemple 5
+// Exemple 6
 #include <Arduino.h>
-    // Handle de la queue
-    xQueueHandle queue;
-void tacheEnvoi(void *parametres)
+
+SemaphoreHandle_t mutex;
+
+void tache1(void *parametres)
+{
+    int i = 0;
+    while (1)
+    {
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        Serial.printf("Dans la tâche 1 : ");
+        delay(1);
+        Serial.printf("%d\n", i);
+        xSemaphoreGive(mutex);
+        i++;
+        delay(1000);
+    }
+}
+void tache2(void *parametres)
 {
     int i = 100;
     while (1)
     {
-        if (xQueueSend(queue, &i, portMAX_DELAY) == pdPASS)
-        {
-            Serial.printf("Envoi %d\n", i);
-            i++;
-        }
-        else
-        {
-            Serial.printf("Envoi échec\n");
-        }
-        delay(2000);
-    }
-}
-void tacheReception(void *parametres)
-{
-    int i;
-    while (1)
-    {
-        if (xQueueReceive(queue, &i,0) != pdTRUE)
-        {
-            Serial.printf("Réception échec\n");
-        }
-        else
-        {
-            Serial.printf("Réception %d\n", i);
-        }
+         xSemaphoreTake(mutex, portMAX_DELAY);
+        Serial.printf("Dans la tâche 2 : ");
+        delay(1);
+        Serial.printf("%d\n", i);
+        xSemaphoreGive(mutex);
+        i++;
         delay(1000);
     }
 }
 void setup()
 {
+    mutex = xSemaphoreCreateMutex();
+
     Serial.begin(115200);
-    while (!Serial);
+    while (!Serial)
+        ;
     Serial.printf("Départ\n");
-    // Création de la file
-    queue = xQueueCreate(10, sizeof(int));
-    xTaskCreate(tacheEnvoi, "Envoi", 10000, NULL, 1, NULL);         /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
-    xTaskCreate(tacheReception, "Réception", 10000, NULL, 1, NULL); /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
+    xTaskCreate(tache1, "Tâche 1", 10000, NULL, 1, NULL);
+    xTaskCreate(tache2, "Tâche 2", 10000, NULL, 1, NULL);
     vTaskDelete(NULL);
 }
-
 void loop()
 {
     // Ne s'exécute pas
